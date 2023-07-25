f8085e875685f1c225861c11b8f0ecd34107215c
Hanieh Mahdavi <haniehmahdavi26@gmail.com>
2023-07-25 21:44:14 +0330

    remove whitspaces and newlines

--- static/cma.js
+++ static/cma.js
@@ -14,14 +14,12 @@ function displayImage(base64Data) {
 function showTable(data) {
     // Show the table container
     document.getElementById("table-container").style.display = "block";
-
     var n_generation = document.getElementById("n-generation")
     var n_offspring = document.getElementById("n-offspring")
     var n_parent = document.getElementById("n-parent")
     var X = document.getElementById("X")
     var y = document.getElementById("y")
     var execTime = document.getElementById("exec-time")
-
     n_generation.textContent = data.n_offspring;
     n_offspring.textContent = data.n_generation;
     n_parent.textContent = data.n_parent;
@@ -30,7 +28,7 @@ function showTable(data) {
     execTime.textContent = data.exec_time;
 }
 
-function sendDataToServer(data, dimension) {
+function sendDataToServer(data) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "/cma", true);
     xhr.setRequestHeader("Content-Type", "application/json");
@@ -42,7 +40,7 @@ function sendDataToServer(data, dimension) {
             if (xhr.status === 200) {
                 var result = JSON.parse(xhr.responseText);
                 showTable(result.table_data);
-                if (dimension == 2) {
+                if (data.dimension == 2) {
                     showChart(result.chart_data);
                 }
             } else {
@@ -74,5 +72,5 @@ document.getElementById("submit").addEventListener("click", function (event) {
         benchmark_function: benchmarkFunction
     };
     // Send the data to the server
-    sendDataToServer(data, dimension);
+    sendDataToServer(data);
 });
